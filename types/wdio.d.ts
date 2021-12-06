15c42b48ecb370b8a10876ef45eed42b2696bb43
rytikovroman1994 <bboycooler@yandex.ru>
2021-12-06 20:34:15 +0300

    add settings

---
+++ types/wdio.d.ts
@@ -0,0 +1,35 @@
+interface IAddTextInField {
+    element: WebdriverIO.Element,
+    text: string,
+    type: 'value' | 'text',
+    needCheckTest?: boolean
+}
+
+interface IOption {
+    button?: "left" | "middle" | "right",
+    x?: number,
+    y?: number
+}
+
+interface IClickElement {
+    element: WebdriverIO.Element,
+    option?: IOption,
+    errorText?: string,
+    log?: boolean
+}
+
+declare namespace WebdriverIO {
+	//   interface Config {
+	//   }
+
+	//   interface Element {
+	//   }
+
+	interface Browser {
+        helper: {
+            AddTextInField: ({ element, text, needCheckTest }: IAddTextInField) => Promise<void>;
+            AllureStep: (name: string, callback: () => Promise<void>) => Promise<void>,
+            ClickElement: ({ element, option, errorText, log }: IClickElement) => Promise<void>
+        }
+	}
+}
